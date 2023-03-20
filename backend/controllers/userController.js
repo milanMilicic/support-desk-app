f6f7c473b3aac18300c1f3111d22fe7dcc91260a
milanMilicic <milanm.milicic@gmail.com>
2023-03-20 18:01:27 +0100

    Implement jsonwebtoken

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require('express-async-handler')
 const bcrypt = require('bcryptjs')
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
 
 const registerUser = asyncHandler(async (req, res) => {
     
@@ -34,7 +35,8 @@ const registerUser = asyncHandler(async (req, res) => {
         res.status(201).json({
             _id: user._id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateToken(user._id)
         })
     } else {
         res.status(400);
@@ -43,8 +45,30 @@ const registerUser = asyncHandler(async (req, res) => {
 })
 
 const loginUser = asyncHandler(async (req, res) => {
-        res.send('Login Route');
+    const {email, password} = req.body;
+
+    const user = await User.findOne({email});
+
+    //Check user and password match
+    if(user && (await bcrypt.compare(password, user.password))){
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(401);
+        throw new Error('Invalid credentials');
+    }
 })
+
+//Generate token
+function generateToken(id){
+    return jwt.sign({id}, process.env.JWT_SECRET, {
+        expiresIn: '30d'
+    });
+}
 
 module.exports = {
     registerUser,
