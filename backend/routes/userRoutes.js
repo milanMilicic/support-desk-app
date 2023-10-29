d475655f5458741af8d7c855779e32e41c79f55c
milanMilicic <milanm.milicic@gmail.com>
2023-10-29 19:26:34 +0100

    Admin route - get all users

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,6 +7,7 @@ router.post('/', registerUser);
 router.post('/login', loginUser);
 router.get('/me', protect, getMe);  //protected route
 
-
+//Admin route
+router.get('/admin/users', protect, checkAdmin, getUsers); //protected route + admin route
 
 module.exports = router;
