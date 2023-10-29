93521e5cdf95196ec90f20dbe915247c9591dc45
milanMilicic <milanm.milicic@gmail.com>
2023-10-29 20:28:05 +0100

    Display notes authors name through note state instead of user state

--- frontend/src/components/NoteItem.jsx
+++ frontend/src/components/NoteItem.jsx
@@ -1,13 +1,10 @@
-import { useSelector } from "react-redux"
-
 function NoteItem({note}) {
-    const {user} = useSelector(state => state.auth)
   return (
     <div className="note" style={{
         backgroundColor: note.isAdmin ? 'rgba(0,0,0,0.7)' : '#fff',
         color: note.isAdmin ? '#fff' : '#000'
     }}>
-        <h4>Note from {note.isAdmin ? <span>Admin</span> : <span>{user.name}</span>}</h4>
+        <h4>Note from {note.isAdmin ? <span>Admin</span> : <span>{note.name}</span>}</h4>
         <p>{note.text}</p>
         <div className="note-date">
             {new Date(note.createdAt).toLocaleString('en-GB')}
