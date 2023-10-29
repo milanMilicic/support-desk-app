b36f9b0cb07e7ff02bdb16b8ca1e7f96a71b018a
milanMilicic <milanm.milicic@gmail.com>
2023-10-29 20:18:17 +0100

    Allow only admin and user, who made a note, to see notes and create notes

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -16,14 +16,66 @@ const getNotes = asyncHandler(async (req, res) => {
 
     const ticket = await Ticket.findById(req.params.ticketId);
 
-    if(ticket.user.toString() !== req.user.id){
-        res.status(401)
-        throw new Error('User not authorizes');
+
+    /* Allow admin and user, who made a note, to see notes */
+    if(req.user.isAdmin) {
+        const notes = await Note.find({ticket: req.params.ticketId});
+        const userIds = notes.map(note => note.user);
+
+        const firstAuthor = await User.find(userIds[0]);
+        const firstAuthorName = firstAuthor[0].name;
+
+         const secondAuthor = await User.find(userIds[1]);
+         const secondAuthorName = secondAuthor[0].name;
+
+         const updatedNotes = notes.map(note => {
+            if(note.user.toString() === userIds[0].toString()){
+                return {
+                    ...note._doc,
+                    name: firstAuthorName
+                }
+            } else {
+                return {
+                    ...note._doc,
+                    name: secondAuthorName
+                }
+            }
+         })
+         
+        res.status(200).json(updatedNotes);
+
+    } else if(ticket.user.toString() === req.user.id) {
+        const notes = await Note.find({ticket: req.params.ticketId});
+        const userIds = notes.map(note => note.user);
+
+        const firstAuthor = await User.find(userIds[0]);
+        const firstAuthorName = firstAuthor[0].name;
+
+         const secondAuthor = await User.find(userIds[1]);
+         const secondAuthorName = secondAuthor[0].name;
+
+         const updatedNotes = notes.map(note => {
+            if(note.user.toString() === userIds[0].toString()){
+                return {
+                    ...note._doc,
+                    name: firstAuthorName,
+                }
+            } else {
+                return {
+                    ...note._doc,
+                    name: secondAuthorName,
+                }
+            }
+         })
+         
+        res.status(200).json(updatedNotes);
+
+    } else {
+        res.status(401);
+        throw new Error('Not authorized');
     }
 
-    const notes = await Note.find({ticket: req.params.ticketId})
-
-    res.status(200).json(notes);
+    
 })
 
 
@@ -38,19 +90,18 @@ const addNote = asyncHandler(async (req, res) => {
 
     const ticket = await Ticket.findById(req.params.ticketId);
 
-    if(ticket.user.toString() !== req.user.id){
-        res.status(401)
-        throw new Error('User not authorizes');
+    if(req.user.isAdmin || ticket.user.toString() === req.user.id){
+        const note = await Note.create({
+            text: req.body.text,
+            isAdmin: req.user.isAdmin,
+            ticket: req.params.ticketId,
+            user: req.user.id
+        })
+    
+        res.status(200).json(note);
     }
 
-    const note = await Note.create({
-        text: req.body.text,
-        isAdmin: false,
-        ticket: req.params.ticketId,
-        user: req.user.id
-    })
-
-    res.status(200).json(note);
 })
+
 
 module.exports = {getNotes, addNote};
